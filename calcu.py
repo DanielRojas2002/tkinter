aeb84c0a05225eaa26d2ebdc633593658be5ffc5
danymessi1000@gmail.com <danymessi1000@gmail.com>
2021-01-10 16:33:27 -0600

    Le agregue mas botones

--- calcu.py
+++ calcu.py
@@ -1,39 +1,85 @@
 from tkinter import Tk,Label,Button,Entry
-
+indice=0
+listaOperaciones=[]
+Ccontador=0
 
 
 def funsuma():
     n1=caja1.get()
     n2=caja2.get() 
     r=float(n1)+float(n2)
+    listaOperaciones.append(n1+" + "+n2+" = "+str(r))
     caja3.delete(0,'end')
     caja3.insert(0,r)
+
+    global indice
+    indice=indice+1
+    caja4.delete(0,"end")
+    caja4.insert(0,indice)
+    global Ccontador
+    Ccontador=Ccontador+1
     
 def funresta():
     n1=caja1.get()
     n2=caja2.get()
     r=float(n1)-float(n2)
+    listaOperaciones.append(n1+" - "+n2+" = "+str(r))
     caja3.delete(0,'end')
     caja3.insert(0,r)
+
+    global Ccontador
+    Ccontador=Ccontador+1
+    
+    global indice
+    indice=indice+1
+    caja4.delete(0,"end")
+    caja4.insert(0,indice)
     
 def funmulti():
     n1=caja1.get()
     n2=caja2.get()
     r=float(n1)*float(n2)
+    listaOperaciones.append(n1+" / "+n2+" = "+str(r))
     caja3.delete(0,"end")
     caja3.insert(0,r)
+
+    global Ccontador
+    Ccontador=Ccontador+1
+    
+    global indice
+    indice=indice+1
+    caja4.delete(0,"end")
+    caja4.insert(0,indice)
+
+
     
 def fundiv():
     n1=caja1.get()
     n2=caja2.get()
     r=float(n1)/float(n2)
+    listaOperaciones.append(n1+" X "+n2+" = "+str(r))
     caja3.delete(0,"end")
     caja3.insert(0,r)
+
+    global Ccontador
+    Ccontador=Ccontador+1
+    
+    global indice
+    indice=indice+1
+    caja4.delete(0,"end")
+    caja4.insert(0,indice)
     
 def funborrar():
     caja1.delete(0,"end")
     caja2.delete(0,"end")
     caja3.delete(0,"end")
+
+def funborrarn1():
+    caja1.delete(0,"end")
+    
+def funborrarn2():
+    caja2.delete(0,"end")
+
 
 ventana=Tk()
 ventana.title("CALCULADORA DE OPERACIONES BASICAS")
@@ -70,8 +116,33 @@ boton4=Button(ventana,text="DIVISION",command=fundiv)
 boton4.place(x=400,y=100,width=100,height=30)
 
 boton5=Button(ventana,text="Borrar Todo",command=funborrar)
-boton5.place(x=250,y=250,width=100,height=30)
+boton5.place(x=450,y=250,width=100,height=30)
 
+boton6=Button(ventana,text="Borrar Numero1",command=funborrarn1)
+boton6.place(x=50,y=250,width=100,height=30)
 
+boton7=Button(ventana,text="Borrar Numero2",command=funborrarn2)
+boton7.place(x=250,y=250,width=100,height=30)
+
+txt4=Label(ventana,text="No de Operaciones Hechas : ",bg="gray")
+txt4.place(x=270,y=140,width=160,height=30)
+
+caja4=Entry(ventana,bg="gray")
+caja4.place(x=450,y=140,width=30,height=30)
 
 ventana.mainloop()
+
+ventana2=Tk()
+ventana2.title("REGISTRO DE OPERACIONES")
+ventana2.geometry("300x300")
+
+contador=1
+for elemento in listaOperaciones:
+    txt1=Label(ventana2,text=elemento,bg="yellow")
+    txt1.grid(row=contador,column=5)
+    contador=contador+1
+    
+txt2=Label(ventana2,text=(f"Numero de Operaciones : {Ccontador} "),bg="yellow")
+txt2.grid(row=contador,column=5)
+
+ventana2.mainloop()
