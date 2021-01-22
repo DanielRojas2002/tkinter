1b28ce733598f616873e5eeffb5f82732532e911
danymessi1000@gmail.com <danymessi1000@gmail.com>
2021-01-21 21:46:47 -0600

    Cmabios

--- POO_mysql.py
+++ POO_mysql.py
@@ -16,43 +16,43 @@ class Aplicacion():
         
 
         self.txt0=tk.Label(self.frame,text="EMPLEADO : ",bg="peach puff")
-        self.txt0.place(x=250,y=30,width=100,height=40)
+        self.txt0.place(x=250,y=30,width=120,height=40)
 
         self.txt1=tk.Label(self.frame,text="Matricula : ",bg="sky blue")
         self.txt1.place(x=40,y=110,width=100,height=40)
 
         self.caja1=tk.Entry(self.frame)
-        self.caja1.place(x=250,y=110,width=100,height=40)
+        self.caja1.place(x=250,y=110,width=120,height=40)
 
         self.txt2=tk.Label(self.frame,text="Nombre : ",bg="sky blue")
         self.txt2.place(x=40,y=170,width=100,height=40)
 
         self.caja2=tk.Entry(self.frame)
-        self.caja2.place(x=250,y=170,width=100,height=40)
+        self.caja2.place(x=250,y=170,width=120,height=40)
 
         self.txt3=tk.Label(self.frame,text="Apellidos : ",bg="sky blue")
         self.txt3.place(x=40,y=230,width=100,height=40)
 
         self.caja3=tk.Entry(self.frame)
-        self.caja3.place(x=250,y=230,width=100,height=40)
+        self.caja3.place(x=250,y=230,width=120,height=40)
 
         self.txt4=tk.Label(self.frame,text="Edad : ",bg="sky blue")
         self.txt4.place(x=40,y=290,width=100,height=40)
 
         self.caja4=tk.Entry(self.frame)
-        self.caja4.place(x=250,y=290,width=100,height=40)
+        self.caja4.place(x=250,y=290,width=120,height=40)
 
         self.txt5=tk.Label(self.frame,text="Telefono : ",bg="sky blue")
         self.txt5.place(x=40,y=350,width=100,height=40)
 
         self.caja5=tk.Entry(self.frame)
-        self.caja5.place(x=250,y=350,width=100,height=40)
+        self.caja5.place(x=250,y=350,width=120,height=40)
 
         self.txt6=tk.Label(self.frame,text="Domicilio : ",bg="sky blue")
         self.txt6.place(x=40,y=410,width=100,height=40)
 
         self.caja6=tk.Entry(self.frame)
-        self.caja6.place(x=250,y=410,width=100,height=40)
+        self.caja6.place(x=250,y=410,width=120,height=40)
 
         self.frame2=tk.Frame(self.ventana,bg="deep sky blue")
         self.frame2.place(x=380,y=410,width=230,height=200)
@@ -84,12 +84,26 @@ class Aplicacion():
 
     def Registrar(self):
         try:
-            clave=self.caja1.get()
-            nombre=self.caja2.get()
-            apellido=self.caja3.get()
-            edad=self.caja4.get()
-            telefono=self.caja5.get()
-            domicilio=self.caja6.get()
+            clave=(self.caja1.get()is not NULL)
+            nombre=(self.caja2.get()is not NULL)
+            apellido=(self.caja3.get()is not NULL)
+            edad=(self.caja4.get()is not NULL)
+            telefono=(self.caja5.get() is not NULL)
+            domicilio=(self.caja6.get() is not NULL)
+            
+
+            try:
+                with sqlite3.connect("Empleados.db") as conn:
+                    c = conn.cursor()
+                    c.execute("CREATE TABLE IF NOT EXISTS registro (clave INTEGER PRIMARY KEY, nombre TEXT NOT NULL, apellido TEXT NOT NULL,edad INTEGER NOT NULL,telefono INTEGER NOT null,domicilio TEXT NOT NULL );")
+                    valores={"clave":clave,"nombre":nombre,"apellido":apellido,"edad":edad,"telefono":telefono,"domicilio":domicilio}
+                    c.execute("INSERT INTO registro VALUES (:clave,:nombre,:apellido,:edad,:telefono,:domicilio)",valores)
+                    
+            except Error as e:
+                print("-"*30)
+                print(f"ERROR :{e}")
+                print("No se registro el registro")
+                print("-"*30)
         except:
             self.Borrar_Todo()
             self.caja1.insert(0,'Llena todos los Datos :)')
@@ -99,18 +113,7 @@ class Aplicacion():
             self.caja5.insert(0,'Llena todos los Datos :)')
             self.caja6.insert(0,'Llena todos los Datos :)')
             
-        try:
-            with sqlite3.connect("Empleados.db") as conn:
-                c = conn.cursor()
-                c.execute("CREATE TABLE IF NOT EXISTS registro (clave INTEGER PRIMARY KEY, nombre TEXT NOT NULL, apellido TEXT NOT NULL,edad INTEGER NOT NULL,telefono INTEGER NOT null,domicilio TEXT NOT NULL );")
-                valores={"clave":clave,"nombre":nombre,"apellido":apellido,"edad":edad,"telefono":telefono,"domicilio":domicilio}
-                c.execute("INSERT INTO registro VALUES (:clave,:nombre,:apellido,:edad,:telefono,:domicilio)",valores)
-                
-        except Error as e:
-            print("-"*30)
-            print(f"ERROR :{e}")
-            print("No se registro el registro")
-            print("-"*30)
+        
 
     def Borrar(self):
         pass
