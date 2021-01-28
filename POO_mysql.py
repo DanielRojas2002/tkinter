54276f4274cc7aa29f5290cd62463202d4c7942d
danymessi1000@gmail.com <danymessi1000@gmail.com>
2021-01-28 15:03:18 -0600

    txtape

--- POO_mysql.py
+++ POO_mysql.py
@@ -140,6 +140,35 @@ class Aplicacion():
             self.boton11.place(x=40,y=130,width=120,height=30)
             self.ventana5.mainloop()
 
+        elif self.combo.get()=="Apellido":
+            self.ventana5=tk.Tk()
+            self.ventana5.title("TXT : ")
+            self.ventana5.iconbitmap("txt.ico")
+            self.ancho_ventana = 300
+            self.alto_ventana = 300
+
+            self.x_ventana = self.ventana5.winfo_screenwidth() - 810 - self.ancho_ventana // 2
+            self.y_ventana = self.ventana5.winfo_screenheight() // 2 - self.alto_ventana // 2
+
+            self.posicion = str(self.ancho_ventana) + "x" + str(self.alto_ventana) + "+" + str(self.x_ventana) + "+" + str(self.y_ventana)
+            self.ventana5.geometry(self.posicion)
+
+            self.ventana5.geometry("200x200")
+            self.ventana5.maxsize(200, 200)
+            self.ventana5.minsize(200, 200)
+
+            self.frame4=tk.Frame(self.ventana5,bg="springgreen4")
+            self.frame4.pack(expand=True,fill="both")
+
+            self.txt001=tk.Label(self.frame4,text="Apellido : ",bg="olivedrab1")
+            self.txt001.place(x=30,y=30,width=140,height=30)
+
+            self.caja000=tk.Entry(self.frame4)
+            self.caja000.place(x=50,y=80,width=100,height=30)
+
+            self.boton11=tk.Button(self.frame4,text="PASARLO A TXT",command=self.APELLIDOTXT,bd=5)
+            self.boton11.place(x=40,y=130,width=120,height=30)
+            self.ventana5.mainloop()
 
 
     def titulo(self):
@@ -158,13 +187,11 @@ class Aplicacion():
         archivoA.close()
 
     def NOMBRETXT(self):
-        contador=0
-        self.titulo()
-        nom=self.caja000.get()
-        nombre=nom.capitalize()
-        archivoA=open("C:\\comun\\Reporte.txt" , 'a')
-        archivoA.write("\n")
         try:
+            contador=0
+            nom=str(self.caja000.get())
+            nombre=nom.capitalize()
+        
             with sqlite3.connect("Empleados.db") as conn:
                 c = conn.cursor()
                 valor={"nombre":nombre}
@@ -179,20 +206,58 @@ class Aplicacion():
                     messagebox.showerror(message="No se encontro el Registro o \n Ingreso un dato Incorrecto",title="ERROR")
                 
                 else:
+                    self.titulo()
+                    archivoA=open("C:\\comun\\Reporte.txt" , 'a')
+                    archivoA.write("\n")
+                    archivoA.write("-"*20+str(nombre)+"-"*20)
+                    archivoA.write("\n")
                     for conjunto in registros:
                         self.datos.append(conjunto)
                     archivoA.write(tabulate(self.datos))
                     archivoA.close()
                     self.datos=[]
-                messagebox.showinfo(message="Su Archivo TXT fue generado en C:comun",title=":)")
+                    messagebox.showinfo(message="Su Archivo TXT fue generado en C:comun",title=":)")
         except:
-            print("dd")
+            print("ERROR")
+
+
+    def APELLIDOTXT(self):
+        try:
+            contador=0
+            ape=str(self.caja000.get())
+            apellido=ape.capitalize()
+        
+            with sqlite3.connect("Empleados.db") as conn:
+                c = conn.cursor()
+                valor={"apellido":apellido}
+                c.execute("SELECT * FROM registro WHERE apellido = :apellido" , valor)
+                registros=c.fetchall()
+
+            
+                for elemento in registros:
+                    contador=contador+1
+            
+                if contador==0:
+                    messagebox.showerror(message="No se encontro el Registro o \n Ingreso un dato Incorrecto",title="ERROR")
+                
+                else:
+                    self.titulo()
+                    archivoA=open("C:\\comun\\Reporte.txt" , 'a')
+                    archivoA.write("\n")
+                    archivoA.write("-"*20+str(apellido)+"-"*20)
+                    archivoA.write("\n")
+                    for conjunto in registros:
+                        self.datos.append(conjunto)
+                    archivoA.write(tabulate(self.datos))
+                    archivoA.close()
+                    self.datos=[]
+                    messagebox.showinfo(message="Su Archivo TXT fue generado en C:comun",title=":)")
+        except:
+            print("ERROR")
+
+
         
 
-
-
-    def Apelltxt(self):
-        pass
 
     def Edadtxt(self):
         pass
