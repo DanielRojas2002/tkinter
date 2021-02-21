da8b5775e46edf64d72cfce08a0d8ca992d608d4
danymessi1000@gmail.com <danymessi1000@gmail.com>
2021-02-20 23:20:44 -0600

    cambio

--- PIA Tecnicas.py
+++ PIA Tecnicas.py
@@ -1,3 +1,99 @@
+import sys 
+import datetime
+import tkinter as tk
+from tkinter import ttk
+import tkinter.font as tkFont
+from tkinter import messagebox
+
+class Aplicacion():
+    def __init__(self):
+        self.ventanai=tk.Tk()
+        self.ancho_ventana = 600
+        self.alto_ventana = 600
+
+        self.x_ventana = self.ventanai.winfo_screenwidth() - 1050 - self.ancho_ventana // 2
+        self.y_ventana = self.ventanai.winfo_screenheight() // 2 - self.alto_ventana // 2
+
+        self.posicion = str(self.ancho_ventana) + "x" + str(self.alto_ventana) + "+" + str(self.x_ventana) + "+" + str(self.y_ventana)
+        self.ventanai.geometry(self.posicion)
+        self.ventanai.title("BIENVENIDO AL Convertidor de Numeros: ")
+        self.ventanai.geometry("600x600")
+        self.ventanai.maxsize(600, 600)
+        self.ventanai.minsize(600, 600)
+        self.fontStyle = tkFont.Font(family="Lucida Grande", size=20)
+        self.ventanai.iconbitmap("Multimedia\\icono.ico")
+
+        self.frame=tk.Frame(self.ventanai,bg="slate gray")
+        self.frame.pack(expand=True,fill="both")
+
+        self.framee=tk.Frame(self.ventanai,bg="royal blue")
+        self.framee.place(x=0,y=0,width=600,height=120)
+
+        self.txt0101=tk.Label(self.framee,text="Conversor de Numeros",background="gold",font=self.fontStyle)
+        self.txt0101.place(x=40,y=10,width=500,height=100)
+
+    
+        self.image=tk.PhotoImage(file="Multimedia\\base.gif")
+        self.txt=tk.Label(image=self.image)
+        self.txt.place(x=85,y=150)
+
+
+        self.frame2=tk.Frame(self.ventanai,bg="deep sky blue")
+        self.frame2.place(x=0,y=480,width=600,height=130)
+        self.frame2.config(cursor="hand1")
+
+        self.botonAlta=tk.Button(self.frame2,text="DECIMAL",command=self.Decimal,bd=5)
+        self.botonAlta.place(x=120,y=40,width=100,height=30)
+
+        self.boton2=tk.Button(self.frame2,text="BINARIO",command=self.binario,bd=5)
+        self.boton2.place(x=360,y=40,width=100,height=30)
+
+
+
+        self.ventanai.mainloop()
+
+    def Decimal(self):
+        self.ventana=tk.Tk()
+        self.ancho_ventana = 200
+        self.alto_ventana = 200
+        self.ventana.iconbitmap("Multimedia\\filtro.ico")
+
+        self.x_ventana = self.ventana.winfo_screenwidth() - 610 - self.ancho_ventana // 2
+        self.y_ventana = self.ventana.winfo_screenheight() // 2 - self.alto_ventana // 2
+
+        self.posicion = str(self.ancho_ventana) + "x" + str(self.alto_ventana) + "+" + str(self.x_ventana) + "+" + str(self.y_ventana)
+        self.ventana.geometry(self.posicion)
+
+        self.ventana.title("OPCIONES")
+        self.ventana.maxsize(200, 200)
+        self.ventana.minsize(200, 200)
+        self.ventana.geometry("200x200")
+
+        self.frame=tk.Frame(self.ventana,bg="red2")
+        self.frame.pack(expand=True,fill="both")
+
+        self.label=tk.Label(self.frame,text="FUNCIONES : ",bg="gold2")
+        self.label.place(x=50,y=20,width=100,height=30)
+
+        self.combo=ttk.Combobox(self.frame)
+        self.combo.place(x=30,y=100)
+        self.combo["values"]=("Binario","Octagonal","Hexagonal")
+
+        self.boton01=tk.Button(self.frame,text="CHECAR",command=self.checar,bd=5)
+        self.boton01.place(x=30,y=160,width=140,height=30)
+        self.ventana.mainloop()
+        
+    def checar(self):
+        pass
+
+    def binario(self):
+        pass
+
+
+app = Aplicacion()
+
+
+
 
 def BinarioADecimalAOCyHE(numero):
     cadena=numero[::-1]
