45a89af717d973764e9562c7575c7e6356a7dc8a
danymessi1000@gmail.com <danymessi1000@gmail.com>
2021-03-05 17:37:40 -0600

    cambio_grafico

--- tkinter_csv.py
+++ tkinter_csv.py
@@ -190,25 +190,34 @@ class Aplicacion():
             self.ventana=tk.Tk()
             self.ventana.title("Graficos")
             self.ancho_ventana = 400
-            self.alto_ventana = 400
+            self.alto_ventana = 200
 
-            self.x_ventana = self.ventanai.winfo_screenwidth() - 500 - self.ancho_ventana // 2
+            self.x_ventana = self.ventanai.winfo_screenwidth() - 300 - self.ancho_ventana // 2
             self.y_ventana = self.ventanai.winfo_screenheight() // 2 - self.alto_ventana // 2
 
             self.posicion = str(self.ancho_ventana) + "x" + str(self.alto_ventana) + "+" + str(self.x_ventana) + "+" + str(self.y_ventana)
             self.ventana.geometry(self.posicion)
         
-            self.ventana.maxsize(400, 400)
-            self.ventana.minsize(400, 400)
+            self.ventana.maxsize(400, 200)
+            self.ventana.minsize(400, 200)
 
             self.frame=tk.Frame(self.ventana,bg="slate gray")
             self.frame.pack(expand=True,fill="both")
+
+            self.txt0=tk.Label(self.frame,text="Ingrese los Datos para poder Graficar :)")
+            self.txt0.place(x=80,y=20,width=250,height=30)
             
             self.txt1=tk.Label(self.frame,text="Eliga la etiqueta del Nombre:")
-            self.txt1.place(x=20,y=20,width=150,height=30)
+            self.txt1.place(x=20,y=60,width=180,height=30)
 
-            self.caja1=tk.Entry(self.frame)
-            self.caja1.place(x=20,y=60,width=150,height=30)
+            self.cajanom=tk.Entry(self.frame)
+            self.cajanom.place(x=230,y=60,width=120,height=30)
+
+            self.txt2=tk.Label(self.frame,text="Eliga la etiqueta del Valor:")
+            self.txt2.place(x=20,y=110,width=180,height=30)
+
+            self.cajaval=tk.Entry(self.frame)
+            self.cajaval.place(x=230,y=110,width=120,height=30)
 
             self.ventana.mainloop()
         
